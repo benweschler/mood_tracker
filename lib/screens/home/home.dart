ed205b011b0a4b9d2ee73eb69e925096e13f130f
Ben Weschler <benjaminweschler@gmail.com>
2022-11-01 23:10:13 -0700

    Made timeline view lazy-loaded.

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -58,91 +58,111 @@ class TimelineView extends StatelessWidget {
     final model = context.watch<MoodEntryModel>();
     final List<DateTime> dates = model.dates.toList()..sort();
 
-    if (dates.isEmpty) {
-      return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: Insets.offset),
-        child: Center(
-          child: Text.rich(
-            TextSpan(children: [
-              const TextSpan(
-                text:
-                    "It looks like you don't have any entries\nAdd an entry to get started ",
-                style: TextStyles.body1,
-              ),
-              EmojiTextSpan(
-                text: "😊",
-                style: TextStyles.body1,
-              ),
-            ]),
-            textAlign: TextAlign.center,
-          ),
-        ),
-      );
-    }
-
-    //TODO: this is a inefficient bad way to build children. Use ListView.builder.
-
-    final List<Widget> children = [];
-
-    DateTime? previousDate;
-    for (DateTime date in dates) {
-      if (previousDate == null || previousDate.month != date.month) {
-        final monthHeading = Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text.rich(TextSpan(children: [
-              TextSpan(
-                text: "${DateFormat.MMMM().format(date)} ",
-                style: TextStyles.heading.copyWith(fontWeight: FontWeight.bold),
-              ),
-              TextSpan(
-                text: DateFormat.y().format(date),
-                style: TextStyles.heading.copyWith(color: AppColors.mutedColor),
-              ),
-            ])),
-            ResponsiveStrokeButton(
-              onTap: () => context.push(
-                const LoggerView(),
-                rootNavigator: true,
-              ),
-              child: const StyledIcon(
-                icon: Icons.timeline,
-                color: AppColors.contrastColor,
-              ),
-            ),
-          ],
-        );
-        children.add(monthHeading);
-      }
-      final bool isMissedDay = previousDate != null
-          ? previousDate.isSameDay(date.copyWith(day: date.day - 1))
-          : false;
-      if (isMissedDay) {
-        children.add(Center(
-          child: Text(
-            "${date.difference(previousDate).inDays} days missing",
-            style: TextStyles.body1.copyWith(color: AppColors.mutedColor),
-          ),
-        ));
-      }
-
-      children.add(EntryDetailsCard(model.entryOn(date)!));
-
-      previousDate = date;
-    }
+    if (dates.isEmpty) return _buildNoEntryMessage();
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: Insets.offset),
       child: Consumer<MoodEntryModel>(
-        builder: (_, model, __) => ListView(
-          children: children
-              .separate(const SizedBox(height: Insets.med))
-              .toList()
-            //TODO: this is a hacky fix to add padding to avoid obscuring the ListView with the bottom action button.
-            ..add(SizedBox(
-              height: MediaQuery.of(context).viewPadding.bottom + 50,
-            )),
+        builder: (_, model, __) => ListView.builder(
+          itemCount: dates.length + 1,
+          itemBuilder: (_, index) {
+            //TODO: this is a hacky way to ensure that the ListView is not obscured by the overlaid action button.
+            if (index == dates.length) {
+              final bottomPadding = SizedBox(
+                height: MediaQuery.of(context).viewPadding.bottom + 50,
+              );
+              return bottomPadding;
+            }
+
+            getEntry(day) => model.entryOn(day);
+            return _buildTimelineElement(index, dates, getEntry);
+          },
         ),
+      ),
+    );
+  }
+
+  Widget _buildTimelineElement(
+      int index, List<DateTime> dates, MoodEntry? Function(DateTime) getEntry) {
+    final List<Widget> children = [];
+    final DateTime date = dates[index];
+    final DateTime? previousDate = index > 0 ? dates[index - 1] : null;
+
+    if (previousDate == null || previousDate.month != date.month) {
+      children.add(_buildMonthHeading(date));
+    } else if (previousDate.isSameDay(date.copyWith(day: date.day - 1))) {
+      children.add(Center(
+        child: Text(
+          "${date.difference(previousDate).inDays} days missing",
+          style: TextStyles.body1.copyWith(color: AppColors.mutedColor),
+        ),
+      ));
+    }
+
+    children.add(EntryDetailsCard(getEntry(date)!));
+
+    return Column(
+      children: children
+          .expand((element) => [element, const SizedBox(height: Insets.med)])
+          .toList(),
+    );
+  }
+
+  Widget _buildMonthHeading(DateTime date) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text.rich(TextSpan(children: [
+          TextSpan(
+            text: "${DateFormat.MMMM().format(date)} ",
+            style: TextStyles.heading.copyWith(fontWeight: FontWeight.bold),
+          ),
+          TextSpan(
+            text: DateFormat.y().format(date),
+            style: TextStyles.heading.copyWith(color: AppColors.mutedColor),
+          ),
+        ])),
+        const _ShowEventLogButton(),
+      ],
+    );
+  }
+
+  Widget _buildNoEntryMessage() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: Insets.offset),
+      child: Center(
+        child: Text.rich(
+          TextSpan(children: [
+            const TextSpan(
+              text:
+                  "It looks like you don't have any entries\nAdd an entry to get started ",
+              style: TextStyles.body1,
+            ),
+            EmojiTextSpan(
+              text: "😊",
+              style: TextStyles.body1,
+            ),
+          ]),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
+
+class _ShowEventLogButton extends StatelessWidget {
+  const _ShowEventLogButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveStrokeButton(
+      onTap: () => context.push(
+        const LoggerView(),
+        rootNavigator: true,
+      ),
+      child: const StyledIcon(
+        icon: Icons.timeline,
+        color: AppColors.contrastColor,
       ),
     );
   }
