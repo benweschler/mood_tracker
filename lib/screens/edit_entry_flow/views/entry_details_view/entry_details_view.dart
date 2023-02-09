f94ccc39859191aa47f7913d04a5b082686b7b91
Ben Weschler <benjaminweschler@gmail.com>
2023-02-08 23:06:56 -0800

    Removed bug where overwrite confirmation would always be triggered.

--- lib/screens/edit_entry_flow/views/entry_details_view/entry_details_view.dart
+++ lib/screens/edit_entry_flow/views/entry_details_view/entry_details_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mood_tracker/models/mood_entry_model.dart';
 import 'package:mood_tracker/screens/edit_entry_flow/entry_template.dart';
 import 'package:mood_tracker/theme.dart';
+import 'package:mood_tracker/utils/date_time_utils.dart';
 import 'package:mood_tracker/utils/navigation_utils.dart';
 import 'package:mood_tracker/widgets/buttons/action_button.dart';
 import 'package:mood_tracker/widgets/buttons/responsive_button.dart';
@@ -47,18 +48,22 @@ class EntryDetailsView extends StatelessWidget {
                       final template = context.read<EntryTemplate>();
                       final model = context.read<MoodEntryModel>();
                       pop() => context.pop(rootNavigator: true);
-                      final confirmation = await showPlatformDialog(
-                        context: context,
-                        dialog: const PlatformAlertDialog(
-                          title: "Overwrite Entry",
-                          content:
-                              "There's already an entry on this day. Are you sure you want to overwrite it?",
-                          confirmText: "Yes",
-                          cancelText: "No",
-                          isDestructiveAction: true,
-                        ),
-                      );
-                      if(!confirmation) return;
+
+                      if(model.dates.contains(template.timestamp.toDate())) {
+                        final confirmation = await showPlatformDialog(
+                          context: context,
+                          dialog: const PlatformAlertDialog(
+                            title: "Overwrite Entry",
+                            content:
+                            "There's already an entry on this day. Are you sure you want to overwrite it?",
+                            confirmText: "Yes",
+                            cancelText: "No",
+                            isDestructiveAction: true,
+                          ),
+                        );
+
+                        if(!confirmation) return;
+                      }
                       model.addEntry(template.toEntry());
                       pop();
                     },
